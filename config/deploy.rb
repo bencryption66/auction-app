8352051a1eb6b2bff304d7f4edbfa417bd16f48c
Ben Cryption <bencryption66@gmail.com>
2025-10-11 11:47:22 +0100

    Cap setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
-set :application, "seamine"
-#set :repo_url, "git@github.com:bencryption66/seamine.git"
-set :repo_url, "git@github.com:bencryption66/seamine.git"
+set :application, "auction-app"
+set :repo_url, "git@github.com:bencryption66/auction-app.git"
 # Also works with non-github repos, I roll my own gitolite server
 set :deploy_to, "/home/deploy/#{fetch :application}"
 set :rbenv_prefix, '/usr/bin/rbenv exec' # Cf issue: https://github.com/capistrano/rbenv/iss
@@ -17,23 +16,14 @@ namespace :secrets do
   desc 'Load the secrets'
   task :load do # |t, args|
     on roles(:all) do
-      execute "source $HOME/.secrets"
+      execute "source $HOME/.auction-secrets"
     end
   end
 
   desc 'Replace database.yml'
   task :replace_db_config do # |t, args|
     on roles(:db) do
-      execute "rm #{fetch :release_path}/config/database.yml && ln -s /home/deploy/config/database.yml #{fetch :release_path}/config/database.yml"
-    end
-  end
-end
-
-namespace :secrets do
-  desc 'Load the secrets'
-  task :load do # |t, args|
-    on roles(:all) do
-      execute "source $HOME/.secrets"
+      execute "rm #{fetch :release_path}/config/database.yml && ln -s /home/deploy/config/auction-database.yml #{fetch :release_path}/config/database.yml"
     end
   end
 end
@@ -91,10 +81,8 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  # after  :finishing,    :restart
 end
 
 before 'deploy:check:directories', 'secrets:load'
 before 'deploy:assets:precompile', 'secrets:replace_db_config'
-#before 'deploy:assets:precompile', 'debug:env'
 
