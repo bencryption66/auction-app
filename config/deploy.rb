9be0416ea609adf4d4642e2da7f1fad52c7dcc23
Ben Cryption <bencryption66@gmail.com>
2025-10-11 13:06:23 +0100

    Remove solid

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock "~> 3.19.2"
 
 set :application, "auction-app"
 set :repo_url, "git@github.com:bencryption66/auction-app.git"
